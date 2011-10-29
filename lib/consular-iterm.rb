7a63c9738e791100839b438c80dcb91abf3d74fa
Arthur Chiu <mr.arthur.chiu@gmail.com>
2011-10-29 15:09:38 -0700

    initial commit

---
+++ lib/consular-iterm.rb
@@ -0,0 +1,7 @@
+require "consular-iterm/version"
+
+module Consular
+  module Iterm
+    # Your code goes here...
+  end
+end
---
+++ lib/consular-iterm/version.rb
@@ -0,0 +1,5 @@
+module Consular
+  module Iterm
+    VERSION = "0.0.1"
+  end
+end
